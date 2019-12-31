6242e6d4567bae246d460e9317e4ea1980462084
Vinh Khuatquang <quangvinhkh@gmail.com>
2020-01-01 00:27:41 +0700

    Add files via upload

---
+++ KDe5blendingprj/Mainfrm_KDe5blending.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KDe5blendingprj
+{
+    public partial class Mainfrm_KDe5blending : Form
+    {
+        public Mainfrm_KDe5blending()
+        {
+            InitializeComponent();
+        }
+    }
+}
